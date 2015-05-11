5b1f3b4ee48131c93491b660d443e2d755b505e7
donaldevine <donaldevine@gmail.com>
2015-05-12 00:11:37 +0100

    added routing

--- Framework/ext-modules/psMenu/psMenuController.js
+++ Framework/ext-modules/psMenu/psMenuController.js
@@ -2,8 +2,15 @@
 
 angular.module('psMenu')
     .controller('psMenuController',
-    ['$scope',
-        function ($scope) {
+    ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
+            this.setActiveElement = function(el) {
+                $scope.activeElement = el;
+            };
 
+            this.setRoute = function(route) {
+                $rootScope.$broadcast('ps-menu-item-selected-event',
+                { route: route });
+            };
         }
     ]);
